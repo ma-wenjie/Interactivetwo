46e2bd83af63c1b9c566fc2c44ac820879e0bab5
ma-wenjie <ma.wenjie@cca.edu>
2017-12-16 10:56:20 -0500

    as

--- Proj 1/script.js
+++ Proj 1/script.js
@@ -1,20 +1,24 @@
 $(document).ready(function(){
 
-	n = 1;
+	n = 0;
 	i = 1;
+	s = 1;
+
+	var colors = ['blue', 'red', 'blue', 'orange']
+
 	$("#container").click(function(){
 			$("#container").css({
-			"transform": "scale(1)"
+			"transform": "scale("+s+")"
 		});
-		
+		s -= .01;
 		n++;
 		for(var i = 1; i < n; i++){
 			$("#container").append('<div id="main"><div id="outer-circle"><div id="inner-circle"><div id="center-circle"><div id="content"><div class="background></div></div></div></div></div></div>');
+			$('body').removeClass();
 			
-		
-
 		}
-
+		$('body').addClass(colors[n]);
+		if (n>=4){n=0};
 	});
 
 
